4ae7deb6fca7fa89946949a942f9b9cd36720bd4
Ihor Pidhrabia <56357858+Pidhrabia@users.noreply.github.com>
2024-03-23 02:02:01 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 // Отримуємо елементи DOM
 let navbar = document.querySelector('.navbar');
+let menuIcon = document.querySelector('#menu-icon'); // Отримуємо іконку меню
 
 // Функція для блокування прокрутки
 function disableScroll() {
@@ -11,45 +12,39 @@ function enableScroll() {
     document.body.style.overflow = 'auto';
 }
 
-
+// Функція для закриття меню
+function closeMenu() {
+    navbar.classList.remove('active');
+    enableScroll(); // Відновлюємо прокрутку
+}
 
 // Обробник кліку для іконки меню
-document.querySelector('#menu-icon').onclick = () => {
+menuIcon.addEventListener('click', () => {
     navbar.classList.toggle('active');
     if (navbar.classList.contains('active')) {
         disableScroll(); // Блокуємо прокрутку
     } else {
         enableScroll(); // Відновлюємо прокрутку
     }
-}
-
-
-
-
-// Обробник кліку на весь документ
-document.addEventListener('mousedown', (event) => {
-    // Перевіряємо, чи клік був поза меню та чи меню відображено
-    if (!event.target.closest('.navbar') && navbar.classList.contains('active')) {
-        navbar.classList.remove('active');
-        enableScroll(); // Відновлюємо прокрутку
-    }
 });
 
-
+// Обробник кліку на весь документ
+document.addEventListener('click', (event) => {
+    // Перевіряємо, чи клік був поза меню та чи меню відображено
+    if (!navbar.contains(event.target) && !menuIcon.contains(event.target) && navbar.classList.contains('active')) {
+        closeMenu(); // Закриваємо меню
+    }
+});
 
 // Обробник кліку на посилання в меню
 document.querySelectorAll('.navbar a').forEach(link => {
     link.addEventListener('click', () => {
-        navbar.classList.remove('active');
-        enableScroll(); // Відновлюємо прокрутку
+        closeMenu(); // Закриваємо меню
     });
 });
-
-
 
 let header = document.querySelector('header');
 
 window.addEventListener('scroll', () => {
     header.classList.toggle('shadow', window.scrollY > 0);
 });
-
